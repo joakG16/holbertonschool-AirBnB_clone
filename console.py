11d7072084f4f1607e78e675b2b56dd7a15d7daa
ismael-soler <ismaelsolerferreira@gmail.com>
2022-10-13 15:09:18 -0300

    task 7 - update

--- console.py
+++ console.py
@@ -31,8 +31,8 @@ class HBNBCommand(cmd.Cmd):
 
         # NO ERRORS
         if errorHappened is False:
-            inputArgs = arg.split()
-            newInstance = eval(inputArgs[0])()
+            argsList = arg.split()
+            newInstance = eval(argsList[0])()
             storage.save()
             print(newInstance.id)
 
@@ -47,8 +47,8 @@ class HBNBCommand(cmd.Cmd):
 
         # if no error happened proceed with the program
         if errorHappened is False:
-            inputArgs = arg.split()
-            objKey = f"{inputArgs[0]}.{inputArgs[1]}"
+            argsList = arg.split()
+            objKey = f"{argsList[0]}.{argsList[1]}"
             objDict = storage.all()
 
             print(objDict[objKey])
@@ -56,7 +56,8 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """
         It checks if the arguments
-        are valid, if they are, it deletes the object from the dictionary and saves the
+        are valid, if they are, it deletes the object from
+        the dictionary and saves the
         dictionary to the file
         """
 
@@ -65,8 +66,8 @@ class HBNBCommand(cmd.Cmd):
 
         # if no error happened proceed with the program
         if errorHappened is False:
-            inputArgs = arg.split()
-            objKey = f"{inputArgs[0]}.{inputArgs[1]}"
+            argsList = arg.split()
+            objKey = f"{argsList[0]}.{argsList[1]}"
             objDict = storage.all()
 
             objDict.pop(objKey)
@@ -79,11 +80,11 @@ class HBNBCommand(cmd.Cmd):
         Otherwise only prints the list of objects of the given class
         """
 
-        inputArgs = arg.split()
+        argsList = arg.split()
         errorHappened = False
 
         # We only check the argument if it was given by the user
-        if len(inputArgs) >= 1:
+        if len(argsList) >= 1:
             errorHappened = self.checkArgs(arg, -1)
         if errorHappened is False:
             objDict = storage.all()
@@ -94,11 +95,34 @@ class HBNBCommand(cmd.Cmd):
             for currentObject in objDict.values():
                 # if we have an argument, then we need to filter what
                 # goes into the list. We use 'isinstance' for that.
-                if len(inputArgs) >= 1:
-                    if isinstance(currentObject, eval(inputArgs[0])) == False:
+                if len(argsList) >= 1:
+                    if isinstance(currentObject, eval(argsList[0])) is False:
                         continue
                 objList.append(str(currentObject))
             print(objList)
+
+    def do_update(self, arg):
+        """
+        Takes the arguments from the command line and updates the
+        attribute of the object with
+        the new value.
+
+        :param arg: <class name> <id> <attribute name> "<attribute value>"
+        """
+        # update <class name> <id> <attribute name> "<attribute value>"
+        #          ARG[0]   ARG[1]    ARG[2]          ARG[3]]
+
+        errorHappened = self.checkArgs(arg, 6)
+
+        if errorHappened is False:
+            argsList = arg.split()
+            objAttName = argsList[2]
+            objAttValue = eval(argsList[3])  # casts the actual object type
+            objDict = storage.all()
+            objKey = f"{argsList[0]}.{argsList[1]}"
+
+            objDict[objKey].__dict__[objAttName] = objAttValue
+            storage.save()
 
     def checkArgs(self, newArgs, numberFlag):
         """
@@ -114,16 +138,16 @@ class HBNBCommand(cmd.Cmd):
         2: if the class exists
         3: if the instance id is missing
         4: if the instance exists
-        5:
-        6:
+        5: if the attribute name is missing
+        6: if the value is missing
         Returns True on error, false otherwise
         """
 
         argsList = newArgs.split()
-
         # CHECKS CLASS NAME CONDITIONS
         # if the class name is missing
         if numberFlag >= 1:
+            # REMEMBER len() STARTS FROM 1
             if len(argsList) <= 0:
                 print("** class name missing **")
                 return True
@@ -144,12 +168,23 @@ class HBNBCommand(cmd.Cmd):
         # if the instance exists
         if numberFlag >= 4:
             # tries to find the object in the dictionary of stored objects
-            # using the key. If it is not found, it will print an error message.
+            # using the key. If it is not found, it will print an error message
             objKey = f"{argsList[0]}.{argsList[1]}"
             objDict = storage.all()  # loads the dict. of stored objects
             # if key specified is not found
-            if objDict.get(objKey) == None:
+            if objDict.get(objKey) is None:
                 print("** no instance found **")
+                return True
+
+        # if the attribute name is missing
+        if numberFlag >= 5:
+            if len(argsList) <= 2:
+                print("** attribute name missing **")
+                return True
+        # if the value is missing
+        if numberFlag >= 6:
+            if len(argsList) <= 3:
+                print("** value missing **")
                 return True
 
         return False
@@ -157,3 +192,6 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
+
+# REMEMBER len() STARTS FROM 1
