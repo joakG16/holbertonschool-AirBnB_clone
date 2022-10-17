8867a16bc3797cce4739a77be1e1e66e77bfe689
ismael-soler <ismaelsolerferreira@gmail.com>
2022-10-17 16:12:01 -0300

    readded __file_path test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,11 +7,23 @@ from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
     """Testing FileStorage"""
+
     def test_filestorage(self):
+        """
+        Test filestorage
+        """
         storage = FileStorage()
         storage.all().clear()
         storage.reload()
         self.assertFalse(len(storage.all()) == 0)
+
+    def test_file_path(self):
+        """
+        Test method to check file name
+        """
+        storage = FileStorage()
+        self.assertEqual(storage.path(), "file.json")
+
 
 if __name__ == "__main__":
     unittest.main()
