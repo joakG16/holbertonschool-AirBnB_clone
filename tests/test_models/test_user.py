ac8aee4fcb75cda615ac49ce1b4327cbd89ad780
JoakG16 <4379@holbertonstudents.com>
2022-10-17 14:09:32 +0000

    user test done

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,18 @@
+"""Tests for base class User """
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    ''' unittest class for checking User'''
+    
+    def test_create_user(self):
+        ''' creating user class '''
+        user1 = User()
+        self.assertEqual(user1.email, "")
+        self.assertEqual(user1.password, "")
+        self.assertEqual(user1.first_name, "")
+        self.assertEqual(user1.last_name, "")
+
+if __name__ == '__main__':
+    unittest.main()
